568b76ab2e07a30fa023dad1ed15d4b6baa8c4cf
Bharath-2006-git <bharath2006warrior@gmail.com>
2025-10-04 16:47:36 +0530

    practice

---
+++ Self Study/Python/If_Else/pass.py
@@ -0,0 +1,4 @@
+stored_password = "PayLakhsUseHotspot"
+user_password = input("Enter your password: ")
+if user_password==stored_password: print("Access Granted")
+else: print("Unrecognized")
---
+++ Self Study/Python/If_Else/temp.py
@@ -0,0 +1,4 @@
+t=float(input("Enter temperature: "))
+if (t<10): print("Cold")
+elif (t>=10 and t<25): print("Normal")
+else: print("Hot")
