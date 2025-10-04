6e9a2e9f5dc57109aea6eaf5b3ae632b29d40c80
Bharath-2006-git <bharath2006warrior@gmail.com>
2025-10-04 15:49:09 +0530

    practise

---
+++ Self Study/Python/Basics/input.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+marks = float(input("Enter you marks: "))
+print(name)
+print(age)
+print(marks)
---
+++ Self Study/Python/Basics/variable.py
@@ -0,0 +1,40 @@
+# a="Bharath"
+# b=56
+# c=5.2
+
+# Print the address of varible
+# print(id(a))
+
+# Delete an varible
+# del a 
+# print(id(a)) 
+
+# Print type of varible 
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# Casting
+# x=str(10)
+# y=int(10)
+# z=float(10)
+# print(y)
+# print(z)
+
+# Case Sensitive
+# age=10
+# Age=20
+# print(age,Age)
+
+# Multiple Assignments
+# a=b=c=5
+# x,y,z=6,6,6
+# print(a,b,c)
+# print(x,y,z)
+
+# Integer Division
+# a=6.0
+# b=2.1
+# print(a/b)
+# print(a//b)
+# print(int(a/b))
