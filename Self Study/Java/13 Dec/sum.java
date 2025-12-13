718b8d39b1914ff36ff4cec975db635fe84ee9c2
Bharath-2006-git <bharath2006warrior@gmail.com>
2025-12-13 18:20:04 +0530

    .

---
+++ Self Study/Java/13 Dec/sum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class sum{
+    public static void main(string[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.println("Enter third number: ");
+        int c = sc.nextInt();
+
+
+
+
+    }
+}
