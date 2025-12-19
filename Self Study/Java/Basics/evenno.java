0fa4c9dd3e793a8286de27f61981a8840bbc6144
Polisetti-Bharath <bharath2006warrior@gmail.com>
2025-12-19 23:27:27 +0530

    .

---
+++ Self Study/Java/Basics/evenno.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class evenno {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your number: ");
+        int num = sc.nextInt();
+        int sum=0;
+        while(num>0){
+        int r = num%10;
+        if(r%2==0) sum=sum+r;
+        num = num/10;
+    }
+        System.out.println("\nSum:"+sum);
+        sc.close();
+
+}
+}
