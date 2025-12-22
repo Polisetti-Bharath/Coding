f3e302a144775116b941e3ad4497d76b6fb9a3cd
Polisetti-Bharath <bharath2006warrior@gmail.com>
2025-12-22 23:04:32 +0530

    .

--- Self Study/Java/Basics/fact.java
+++ Self Study/Java/Basics/fact.java
@@ -4,6 +4,10 @@ public class fact{
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of number:");
         int n = sc.nextInt();
-
+        int fact = 1 ;
+        for(int i=1;i<n;i++){
+            fact *=i;
+        }
+        System.out.println("Factorial:"+fact);
     }
 }
