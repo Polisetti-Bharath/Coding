752342b69fd05af0fe560e3c69c5d27e926010de
Polisetti-Bharath <bharath2006warrior@gmail.com>
2025-12-22 23:00:12 +0530

    .

---
+++ Self Study/Java/Basics/fact.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class fact{
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of number:");
+        int n = sc.nextInt();
+
+    }
+}
