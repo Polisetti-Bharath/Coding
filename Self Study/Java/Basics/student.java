cfcca79027c126729c3d5b556b2bc4f573e2110e
Polisetti-Bharath <bharath2006warrior@gmail.com>
2026-01-01 18:33:09 +0530

    .

---
+++ Self Study/Java/Basics/student.java
@@ -0,0 +1,8 @@
+public class student{
+    public static void main(String[] args){
+    int sid = 123;
+    String name = "Bharath";
+    int marks = 50;
+    System.out.println("Welcome:"+sid+" "+name+" "+marks);
+    }
+}
